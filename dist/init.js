2f9008709caf0d45053c91508d2d0f94cd9ed653
BenTheElder <bentheelder@gmail.com>
2016-07-07 16:51:59 -0400

    place canvas in container div and center game with basic stylesheet

--- dist/init.js
+++ dist/init.js
@@ -2,7 +2,11 @@ if ((location.protocol != "https:") && (location.hostname == "capstone.fenichela
     location.protocol = "https:";
 }
 window.onload = function () {
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+    var game = new Phaser.Game({
+        width: 800,
+        height: 600,
+        renderer: Phaser.AUTO,
+        parent: 'game-container',
         create: create,
         preload: preload,
         render: render,
