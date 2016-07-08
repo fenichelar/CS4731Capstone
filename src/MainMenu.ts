bb0d102a2e8ab2afe1e61ad1830e2bcf37e4b850
Alec Fenichel <alec.fenichel@gmail.com>
2016-07-08 13:48:02 -0400

    Add jsdoc with author and license data

--- src/MainMenu.ts
+++ src/MainMenu.ts
@@ -1,3 +1,10 @@
+/**
+ * @author Alec Fenichel <alec.fenichel@gmail.com>
+ * @author Matt Schmidt
+ * @author Benjamin Elder
+ * @license {@link https://github.com/fenichelar/CS4731Capstone/blob/master/LICENSE.md|Apache License 2.0}
+ */
+
 module Game {
   export class MainMenu extends Phaser.State {
 
