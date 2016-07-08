f487b4303389c5e042a13300dfb528061c547115
Alec Fenichel <alec.fenichel@gmail.com>
2016-07-08 12:05:28 -0400

    Merge titlescreen state with preload state

--- src/Preloader.ts
+++ src/Preloader.ts
@@ -4,17 +4,41 @@ module Game {
     preloadBar: Phaser.Sprite;
 
     preload() {
-      this.preloadBar = this.add.sprite(200, 250, "preloadBar");
+      const titleText: string = "Game Title";
+      const titleX: number = this.game.world.centerX;
+      const titleY: number = this.game.world.centerY - this.game.height * 0.25;
+      let title: any = this.game.add.text(titleX, titleY, titleText, {
+        boundsAlignH: "center",
+        boundsAlignV: "middle",
+        fill: "#fff",
+        font: "50px Titillium Web"
+      });
+      title.anchor.setTo(0.5, 0.5);
+
+      this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, "preloadBar");
+      this.preloadBar.anchor.setTo(0.5, 0.5);
       this.load.setPreloadSprite(this.preloadBar);
+
+      // Load assets here
     }
 
     create() {
-      let tween = this.add.tween(this.preloadBar).to({ alpha: 0 }, 1000, Phaser.Easing.Linear.None, true);
-      tween.onComplete.add(this.startTitleScreen, this);
+      const pressKeyText: string = "Press any key to start...";
+      const pressKeyX: number = this.game.world.centerX;
+      const pressKeyY: number = this.game.world.centerY + this.game.height * 0.25;
+      let pressKey: any = this.game.add.text(pressKeyX, pressKeyY, pressKeyText, {
+        boundsAlignH: "center",
+        boundsAlignV: "middle",
+        fill: "#ff0",
+        font: "20px Titillium Web"
+      });
+      pressKey.anchor.setTo(0.5, 0.5);
+
+      this.input.onDown.addOnce(this.startMainMenu, this);
     }
 
-    startTitleScreen() {
-      this.game.state.start("TitleScreen", true, false);
+    startMainMenu() {
+      this.game.state.start("MainMenu");
     }
 
   }
