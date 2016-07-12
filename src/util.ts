c4e4c4d3f548ff599d1a789e56cec28050228f3d
BenTheElder <bentheelder@gmail.com>
2016-07-12 16:49:14 -0400

    move title text to new src/util.ts

---
+++ src/util.ts
@@ -0,0 +1,24 @@
+/**
+ * @author Alec Fenichel <alec.fenichel@gmail.com>
+ * @author Matt Schmidt
+ * @author Benjamin Elder
+ * @license {@link https://github.com/fenichelar/CS4731Capstone/blob/master/LICENSE.md|Apache License 2.0}
+ */
+
+/// <reference path="../node_modules/phaser/typescript/phaser.d.ts" />
+
+namespace Game {
+  export const name: string = "Game Title";
+  export function makeTitle(game: Game.Game): Phaser.Text {
+    const titleX: number = game.world.centerX;
+    const titleY: number = game.world.centerY - game.height * 0.25;
+    let title: any = game.add.text(titleX, titleY, name, {
+      boundsAlignH: "center",
+      boundsAlignV: "middle",
+      fill: "#fff",
+      font: "50px Titillium Web"
+    });
+    title.anchor.setTo(0.5, 0.5);
+    return title;
+  }
+}
