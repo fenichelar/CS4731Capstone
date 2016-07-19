f1601afb9553e065976b9a3d7963383c933ce4a0
Benjamin Elder <bentheelder@gmail.com>
2016-07-19 13:08:59 -0400

    start switching Battle.ts to use the FleetCompGenerator (#23)

--- src/Battle.ts
+++ src/Battle.ts
@@ -16,63 +16,14 @@ namespace Game {
   export class Battle extends Phaser.State {
     static Seed: number = 31337;
     static Difficulty: Difficulty = Difficulty.Easy;
-    private enemies: Array<Phaser.Rectangle>;
-    private obstacles: Array<Phaser.Rectangle>;
+    private EnemyFleetGenerator: FleetCompGenerator;
+    private enemies: Array<Game.Ship>;
 
     preload() {
       // Seed RNG with known seed.
       this.game.rnd.sow([Battle.Seed]);
-      this.generateMap();
-      this.generateEnemies();
-    }
-
-    render() {
-      for (let obstacle of this.obstacles) {
-        this.game.debug.geom(obstacle, "#0fffff");
-      }
-      for (let enemy of this.enemies) {
-        this.game.debug.geom(enemy, "#000fff");
-      }
-    }
-
-    generateMap() {
-      // generate obstacles
-      this.obstacles = new Array<Phaser.Rectangle>();
-      for (let i = 0; i < Battle.Difficulty; i++) {
-        let obstacle: any = this.generateObstacle();
-        this.obstacles.push(obstacle);
-      }
-    }
-
-    generateEnemies() {
-      this.enemies = new Array<Phaser.Rectangle>();
-      // generate enemy fleet
-      for (let i = 0; i < Battle.Difficulty; i++) {
-        let enemy: any = this.generateEnemy();
-        this.enemies.push(enemy);
-      }
-    }
-
-    generateObstacle() {
-      let bounds: Phaser.Rectangle = this.game.world.bounds;
-      let width: number = 50;
-      let height: number = 50;
-      let x: number = this.game.rnd.between(bounds.left + width, bounds.right - width);
-      let y: number = this.game.rnd.between(bounds.bottom + height, bounds.top - height);
-      console.log(x, y);
-      let box: any = new Phaser.Rectangle(x, y, width, height);
-      return box;
-    }
-
-    generateEnemy() {
-      let bounds: Phaser.Rectangle = this.game.world.bounds;
-      let width: number = 50;
-      let height: number = 50;
-      let x: number = this.game.rnd.between(bounds.centerX + width, bounds.right - width);
-      let y: number = this.game.rnd.between(bounds.bottom + height, bounds.top - height);
-      let box: any = new Phaser.Rectangle(x, y, width, height);
-      console.log(x, y);
-      return box;
+      this.EnemyFleetGenerator = new FleetCompGenerator();
+      this.enemies = this.EnemyFleetGenerator.generateFleet();
     }
   }
 }
