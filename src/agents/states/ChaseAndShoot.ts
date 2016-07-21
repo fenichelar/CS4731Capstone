302a441f7d9d28cb0eab8f5208c0f057fdb7e2ac
BenTheElder <bentheelder@gmail.com>
2016-07-21 02:57:28 -0400

    handle null target, start to select enemy

--- src/agents/states/ChaseAndShoot.ts
+++ src/agents/states/ChaseAndShoot.ts
@@ -12,10 +12,16 @@ namespace Game {
             if (this.target == null || this.target.health <= 0) {
                 // select a new target
                 // TODO
+                if (Battle.CurrentBattle != null) {
+                  let enemies: Array<Game.Ship> = Battle.CurrentBattle.enemies;
+                  // TODO: sort and select by distance
+                }
             }
-            ship.turnTowards(this.target);
-            ship.thrust(ship.maxThrustSpeed);
-            ship.fire();
+            if (this.target != null) {
+              ship.turnTowards(this.target);
+              ship.thrust(ship.maxThrustSpeed);
+              ship.fire();
+            }
             return this;
         }
     }
